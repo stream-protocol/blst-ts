ada718ba48df72afda9ee5671a61f0940a890b16
Bryan White <bryanchriswhite@gmail.com>
2022-06-08 21:11:43 +0200

    chore: restructure prebuild dir

--- src/scripts/paths.ts
+++ src/scripts/paths.ts
@@ -3,14 +3,14 @@ import path from "path";
 
 const ROOT_DIR = path.join(__dirname, "../..");
 const BLST_NODE = "blst.node";
-export const PREBUILD_DIR = path.join(ROOT_DIR, "prebuild");
+export const PREBUILD_DIR = path.join(ROOT_DIR, "prebuild", "swig");
 export const PACKAGE_JSON_PATH = path.join(ROOT_DIR, "package.json");
-export const BINDINGS_DIR = path.join(ROOT_DIR, "blst/bindings/node.js");
+export const BINDINGS_DIR = path.join(ROOT_DIR, "blst", "bindings", "node.js");
 
 // Paths for blst_wrap.cpp
 // Resolve path to absolute since it will be used from a different working dir
 // when running blst_wrap.py
-export const BLST_WRAP_CPP_PREBUILD = path.resolve(ROOT_DIR, "prebuild", "blst_wrap.cpp");
+export const BLST_WRAP_CPP_PREBUILD = path.resolve(ROOT_DIR, "prebuild", "swig", "blst_wrap.cpp");
 
 /**
  * Get binary name.
